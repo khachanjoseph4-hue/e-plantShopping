a767953495b455bdae8eb064d65c13dd8f2e0eaa
khachanjoseph4-hue <khachanjoseph4@gmail.com>
2025-12-14 18:14:26 +0200

    Create AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Paradise Nursery</h1>
+      <p>We are passionate about bringing nature closer to you.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
