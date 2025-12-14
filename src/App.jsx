eb8524b125c22b9270e449779297fceb7e302fea
khachanjoseph4-hue <khachanjoseph4@gmail.com>
2025-12-14 14:52:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import "./App.css";
+import AboutUs from "./components/AboutUs";
 
+function App() {
+  return (
+    <div className="App">
+      <h1>Paradise Nursery</h1>
+      <button>Get Started</button>
+      <AboutUs />
+    </div>
+  );
+}
+
+export default App;
