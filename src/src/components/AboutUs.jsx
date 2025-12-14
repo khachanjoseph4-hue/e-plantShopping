99e5111797a7302326ca18890bf1f8273cd0dead
khachanjoseph4-hue <khachanjoseph4@gmail.com>
2025-12-14 15:03:47 +0200

    Create AboutUs.jsx

---
+++ src/src/components/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery offers a wide range of indoor and outdoor plants to enhance your living space. We are committed to helping you grow a greener world.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
